b91458e1119674a1cac389a9aa30c1a73bb9c48c
Kendustin <80982601+Kendustin@users.noreply.github.com>
2021-03-23 14:16:06 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -23,6 +23,8 @@ abstract contract Context {
     }
 }
 
+pragma solidity ^0.6.0;
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract PekerjaaUmum is ERC20 {
